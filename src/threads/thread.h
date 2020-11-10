b94fbc55d3314e994d1ecdc7137e5560d2bebc2f
PanTaixi <tp1119@ic.ac.uk>
2020-11-10 03:09:32 +0000

    Formatted and added ifndef macros to get ready for implementing syscallhandler

--- src/threads/thread.h
+++ src/threads/thread.h
@@ -1,6 +1,13 @@
 #ifndef THREADS_THREAD_H
 #define THREADS_THREAD_H
 
+#ifndef USERPROG
+#define USERPROG
+#endif
+#ifndef FILESYS
+#define FILESYS
+#endif
+
 #include <debug.h>
 #include <list.h>
 #include <stdint.h>
