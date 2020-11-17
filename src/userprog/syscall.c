c4c2a53fe1dbee2b95c76d0159f201553c29ae03
PanTaixi <tp1119@ic.ac.uk>
2020-11-17 12:23:15 +0000

    Fixed some null ptr issue

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -72,20 +72,20 @@ static void check_valid_pointer(void *pointer) {
       pagedir_get_page(t->pagedir, pointer) == NULL) {
     int exit_status = -1;
     syscall_exit_helper(exit_status);
-    // // TODO Design the pointer validation logic
-    // printf("Invalid pointer access!\n");
-    // NOT_REACHED();  // Panic the os to indicate error. Should be replaced by
-    //                 // some handling logic
   }
 }
 
 static int get_syscall_number(struct intr_frame *f) {
   void *stack_ptr = f->esp;
+
   check_valid_pointer(stack_ptr);
   int sys_call_no = *(int *)stack_ptr;
+
   return sys_call_no;
 }
 
+// Thread safe because only the thread itself can
+// access t->opened_files;
 static struct opened_file *get_opened_file(int fd) {
   struct list *opened_files = &thread_current()->opened_files;
   struct list_elem *e;
@@ -110,10 +110,6 @@ static void syscall_handler(struct intr_frame *f) {
   void *arg2 = f->esp + 8;
   void *arg3 = f->esp + 12;
 
-  check_valid_pointer(arg1);
-  check_valid_pointer(arg2);
-  check_valid_pointer(arg3);
-
   syscall_func function = syscall_functions[sys_call_no];
 
   unsigned int result = function(arg1, arg2, arg3);
@@ -131,7 +127,7 @@ static unsigned int syscall_halt(void *arg1 UNUSED, void *arg2 UNUSED,
 
 static unsigned int syscall_exit(void *arg1, void *arg2 UNUSED,
                                  void *arg3 UNUSED) {
-  // check_valid_pointer(arg1);
+  check_valid_pointer(arg1);
   int exit_status = *(int *)arg1;
   syscall_exit_helper(exit_status);
   return 0;  // void
@@ -147,8 +143,12 @@ static void syscall_exit_helper(int exit_status) {
 // let pid = tid
 static unsigned int syscall_exec(void *arg1, void *arg2 UNUSED,
                                  void *arg3 UNUSED) {
-  printf("exec");
+  // DEBUG
+  // printf("syscall_exec\n");
+
+  check_valid_pointer(arg1);
   const char *cmd_line = *(const char **)arg1;
+
   lock_acquire(&filesys_lock);
   tid_t tid = process_execute(cmd_line);
   lock_release(&filesys_lock);
@@ -158,35 +158,55 @@ static unsigned int syscall_exec(void *arg1, void *arg2 UNUSED,
 // haven't completed yet
 static unsigned int syscall_wait(void *arg1, void *arg2 UNUSED,
                                  void *arg3 UNUSED) {
-  printf("wait");
+  // DEBUG
+  // printf("syscall_wait\n");
+
+  check_valid_pointer(arg1);
   pid_t pid = *(pid_t *)arg1;
+
   return process_wait(pid);  // int
 }
 
-// Tested OK
 static unsigned int syscall_create(void *arg1, void *arg2, void *arg3 UNUSED) {
+  check_valid_pointer(arg1);
+  check_valid_pointer(arg2);
   const char *file = *(char **)arg1;
   unsigned int initial_size = *(unsigned int *)arg2;
-  lock_acquire(&filesys_lock);
-  bool result = filesys_create(file, initial_size);
-  lock_release(&filesys_lock);
+
+  bool result;
+  if (file == NULL) {
+    result = false;
+  } else {
+    lock_acquire(&filesys_lock);
+    result = filesys_create(file, initial_size);
+    lock_release(&filesys_lock);
+  }
   return result;  // bool
 }
 
 // Tested OK
 static unsigned int syscall_remove(void *arg1, void *arg2 UNUSED,
                                    void *arg3 UNUSED) {
+  check_valid_pointer(arg1);
   const char *file = *(char **)arg1;
-  lock_acquire(&filesys_lock);
-  bool result = filesys_remove(file);
-  lock_release(&filesys_lock);
+
+  bool result;
+  if (file == NULL) {
+    result = false;
+  } else {
+    lock_acquire(&filesys_lock);
+    result = filesys_remove(file);
+    lock_release(&filesys_lock);
+  }
   return result;  // bool
 }
 
 // Tested OK
 static unsigned int syscall_open(void *arg1, void *arg2 UNUSED,
                                  void *arg3 UNUSED) {
+  check_valid_pointer(arg1);
   const char *file_name = *(char **)arg1;
+
   lock_acquire(&filesys_lock);
   struct file *file = filesys_open(file_name);
   if (!file) {
@@ -219,7 +239,9 @@ static unsigned int syscall_open(void *arg1, void *arg2 UNUSED,
 // Tested OK
 static unsigned int syscall_filesize(void *arg1, void *arg2 UNUSED,
                                      void *arg3 UNUSED) {
+  check_valid_pointer(arg1);
   int fd = *(int *)arg1;
+
   struct opened_file *opened_file = get_opened_file(fd);
 
   if (!opened_file) {
@@ -233,6 +255,9 @@ static unsigned int syscall_filesize(void *arg1, void *arg2 UNUSED,
 }
 
 static unsigned int syscall_read(void *arg1, void *arg2, void *arg3) {
+  check_valid_pointer(arg1);
+  check_valid_pointer(arg2);
+  check_valid_pointer(arg3);
   int fd = *(int *)arg1;
   void *buffer = *(char **)arg2;
   unsigned int size = *(unsigned int *)arg3;
@@ -258,9 +283,13 @@ static unsigned int syscall_read(void *arg1, void *arg2, void *arg3) {
 }
 
 static unsigned int syscall_write(void *arg1, void *arg2, void *arg3) {
+  check_valid_pointer(arg1);
+  check_valid_pointer(arg2);
+  check_valid_pointer(arg3);
   int fd = *(int *)arg1;
   const void *buffer = *(char **)arg2;
   unsigned size = *(unsigned *)arg3;
+
   if (fd == STDOUT_FILENO) {
     putbuf(buffer, size);
     return size;
@@ -282,8 +311,11 @@ static unsigned int syscall_write(void *arg1, void *arg2, void *arg3) {
 }
 
 static unsigned int syscall_seek(void *arg1, void *arg2, void *arg3 UNUSED) {
+  check_valid_pointer(arg1);
+  check_valid_pointer(arg2);
   int fd = *(int *)arg1;
   unsigned int position = *(unsigned int *)arg2;
+
   struct opened_file *opened_file;
 
   lock_acquire(&filesys_lock);
@@ -305,7 +337,9 @@ static unsigned int syscall_seek(void *arg1, void *arg2, void *arg3 UNUSED) {
 
 static unsigned int syscall_tell(void *arg1, void *arg2 UNUSED,
                                  void *arg3 UNUSED) {
+  check_valid_pointer(arg1);
   int fd = *(int *)arg1;
+
   struct opened_file *opened_file;
   unsigned int result;
 
@@ -329,7 +363,9 @@ static unsigned int syscall_tell(void *arg1, void *arg2 UNUSED,
 
 static unsigned int syscall_close(void *arg1, void *arg2 UNUSED,
                                   void *arg3 UNUSED) {
+  check_valid_pointer(arg1);
   int fd = *(int *)arg1;
+
   struct opened_file *opened_file;
   opened_file = get_opened_file(fd);
 
