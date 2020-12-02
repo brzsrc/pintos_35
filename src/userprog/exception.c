320a07b8c32bdc479476bdf25e1d46d49a309c15
PanTaixi <tp1119@ic.ac.uk>
2020-12-02 04:41:55 +0000

    Removed unnecessary printf in page fault handler
    changed gitlab ci yml such that it will run the whole test suite

--- src/userprog/exception.c
+++ src/userprog/exception.c
@@ -158,9 +158,6 @@ static void page_fault(struct intr_frame *f) {
   /* To implement virtual memory, delete the rest of the function
      body, and replace it with code that brings in the page to
      which fault_addr refers. */
-  printf("Page fault at %p: %s error %s page in %s context.\n", fault_addr,
-         not_present ? "not present" : "rights violation",
-         write ? "writing" : "reading", user ? "user" : "kernel");
   if (user) {
     if (not_present) {
       struct spmt_pt_entry *e;
@@ -173,6 +170,9 @@ static void page_fault(struct intr_frame *f) {
         ASSERT(load_page(e->load_details, t, upage));
       }
     } else {
+      printf("Page fault at %p: %s error %s page in %s context.\n", fault_addr,
+             not_present ? "not present" : "rights violation",
+             write ? "writing" : "reading", user ? "user" : "kernel");
       syscall_exit_helper(-1);
     }
   } else {
